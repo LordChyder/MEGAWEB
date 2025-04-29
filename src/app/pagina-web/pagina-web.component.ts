f57f76f38bfd5ffb81c8eb2bca2e9fa7dd7e5bd0
YDER <yder12354@gmail.com>
2025-04-29 13:09:40 -0500

    Creacion de los componentes de ADMINISTRADOR Y PAGINA WEB

---
+++ src/app/pagina-web/pagina-web.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-web',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-web.component.html',
+  styleUrl: './pagina-web.component.css'
+})
+export class PaginaWebComponent {
+
+}
