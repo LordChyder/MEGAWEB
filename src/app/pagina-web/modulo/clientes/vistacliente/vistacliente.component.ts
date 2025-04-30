d54bebb71b0265f27aa2c8d43e32ea5fd662db37
FRETH LOPEZ <zepolfz18@gmail.com>
2025-04-30 10:54:17 -0500

    Modulo VistaCliente

---
+++ src/app/pagina-web/modulo/clientes/vistacliente/vistacliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vistacliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './vistacliente.component.html',
+  styleUrl: './vistacliente.component.css'
+})
+export class VistaclienteComponent {
+
+}
