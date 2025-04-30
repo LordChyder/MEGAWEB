7aad403d45f488acfcd9d87f9d2f8a7cb99ddd81
FRETH LOPEZ <zepolfz18@gmail.com>
2025-04-30 10:51:20 -0500

    Modulo VistaProducto

---
+++ src/app/pagina-web/modulo/productos/vistaproducto/vistaproducto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vistaproducto',
+  standalone: true,
+  imports: [],
+  templateUrl: './vistaproducto.component.html',
+  styleUrl: './vistaproducto.component.css'
+})
+export class VistaproductoComponent {
+
+}
