d4e7e8f07ec1875e2f751a1b93187e0a780d9b85
FRETH LOPEZ <zepolfz18@gmail.com>
2025-04-30 10:52:42 -0500

    Modulo VistaConsulta

---
+++ src/app/pagina-web/modulo/consultas/vistaconsulta/vistaconsulta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vistaconsulta',
+  standalone: true,
+  imports: [],
+  templateUrl: './vistaconsulta.component.html',
+  styleUrl: './vistaconsulta.component.css'
+})
+export class VistaconsultaComponent {
+
+}
