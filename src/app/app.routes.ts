15cb83bb31368ebb96d1107d181ebef2271fa6d2
YDER <yder12354@gmail.com>
2025-05-01 14:36:47 -0500

    estructura de las rutas, y vista login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pagina-web/pagina-web.component').then(m => m.PaginaWebComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./administrador/administrador.component').then(m => m.AdministradorComponent),
+  },
+  {
+    path: '**', // Ruta comodín (por si no coincide con ninguna otra)
+    redirectTo: '', // Redirige a página principal
+    pathMatch: 'full',
+  },
+];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  isLoginMode: boolean = true;
+
+  toggleMode() {
+    this.isLoginMode = !this.isLoginMode;}
+
+    onSubmit() {
+      if (this.isLoginMode) {
+        //this.authService.login(); // marcar como autenticado
+        //this.router.navigate(['/pagina-web']); // redirigir
+      } else {
+        // lógica de registro simulada
+        this.isLoginMode = true; // después de registrar, vuelve al login
+      }
+    }
 
 }
