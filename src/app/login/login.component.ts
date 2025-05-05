be6229370b5ea92be4b003b2055a597551b412ed
YDER <yder12354@gmail.com>
2025-05-05 16:14:42 -0500

    Modulo de login de diseño listo (falta las implementaciones del back y de las validaciones)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
