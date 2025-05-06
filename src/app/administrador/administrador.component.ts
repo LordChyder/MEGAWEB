5647e922724ad26ac728ff518c85ecb61e892b72
YDER <yder12354@gmail.com>
2025-05-06 16:42:40 -0500

    Modulo de inicio del diseño terminado

--- src/app/administrador/administrador.component.ts
+++ src/app/administrador/administrador.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ToolbarComponent } from '../shared/toolbar ADMIN/toolbar.component';
+import { SidebarComponent } from '../shared/sidebar Admin/sidebar.component';
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [],
+  imports: [ToolbarComponent, SidebarComponent ],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.css'
 })
---
+++ src/app/shared/toolbar ADMIN/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css'
+})
+export class ToolbarComponent {
+
+}
