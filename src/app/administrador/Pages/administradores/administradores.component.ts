a071b6610eb8f06ef06edf53db84dc77215e793b
YDER <yder12354@gmail.com>
2025-05-07 15:50:35 -0500

    Modulo de administracion #1 parte echa

--- src/app/administrador/Pages/administradores/administradores.component.ts
+++ src/app/administrador/Pages/administradores/administradores.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+interface Admin {
+  id: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  perfil: string;
+  status: 'ACTIVE' | 'INACTIVE';
+}
 
 @Component({
   selector: 'app-administradores',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './administradores.component.html',
-  styleUrl: './administradores.component.css'
 })
 export class AdministradoresComponent {
-
+  admins: Admin[] = [
+    {
+      id: 1,
+      nombre: 'ANDY H.',
+      apellido: 'Rucoba Reategui',
+      email: 'Soporte@megayuntas.com',
+      perfil: 'ADMINISTRADOR',
+      status: 'ACTIVE'
+    },
+    // …otros registros…
+  ];
 }
