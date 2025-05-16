d9a8402ec721f4615d5d003d540a81f828eb686d
YDER <yder12354@gmail.com>
2025-05-15 22:59:59 -0500

    avance

--- src/app/administrador/Pages/clientes/clientes.component.ts
+++ src/app/administrador/Pages/clientes/clientes.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AgregarClienteModalComponent } from './agregar-cliente-modal/agregar-cliente-modal.component';
+import { EditarClienteModalComponent } from './editar-cliente-modal/editar-cliente-modal.component';
+import { EliminarClienteModalComponent } from './eliminar-cliente-modal/eliminar-cliente-modal.component';
 
   interface Cliente {
   id: number;
@@ -15,18 +17,23 @@ import { AgregarClienteModalComponent } from './agregar-cliente-modal/agregar-cl
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [CommonModule, AgregarClienteModalComponent],
+  imports: [CommonModule, AgregarClienteModalComponent,
+            EditarClienteModalComponent, EliminarClienteModalComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
   mostrarModalAgregar = false;
+  mostrarModalEditar = false;
+  mostrarModalEliminar = false;
+  clienteIdAEliminar: number | null = null
 
         Clientes: Cliente[] = [
     {
       id: 1,
-      Empresa: 'ANDY H.',
+      Empresa: 'CONSTRUCTORA INMOBILIARIA RIO HUALLAGA S.A.C CONSTRUCTORA INMOBILIARIA RIO HUALLAGA S.A.C',
       Ruc: 'Rucoba Reategui',
       Telefono: 'Soporte@megayuntas.com',
       Contacto: 'ADMINISTRADOR',
@@ -36,12 +43,37 @@ export class ClientesComponent {
     },
     // …otros registros…
   ];
-
-    abrirModalAgregar(): void {
+//FUNCION DE AGREGAR CLIENTE
+  abrirModalAgregar(): void {
     this.mostrarModalAgregar = true;
   }
-
   cerrarModalAgregar(): void {
     this.mostrarModalAgregar = false;
   } 
+
+//FUNCION DE EDITAR CLIENTE
+  abrirModalEditar(): void {
+    this.mostrarModalEditar = true;
+  }
+  cerrarModalEditar(): void {
+    this.mostrarModalEditar = false;
+  }
+
+//FUNCION DE ELIMINAR CLIENTE  
+  abrirModalEliminar(clienteId: number): void {
+  this.clienteIdAEliminar = clienteId
+  this.mostrarModalEliminar = true
+  }
+  cerrarModalEliminar(): void {
+    this.mostrarModalEliminar = false
+    this.clienteIdAEliminar = null
+  }
+  eliminarCliente(clienteId: number): void {
+    console.log("Eliminando cliente con ID:", clienteId)
+    // Aquí implementarías la lógica para eliminar el cliente
+    // Por ejemplo:
+    this.Clientes = this.Clientes.filter((cliente) => cliente.id !== clienteId)
+    this.cerrarModalEliminar()
+  }
+
 }
