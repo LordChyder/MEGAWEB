eb76ac77f65057dbc2379cf7637f8d078532efc3
Lordchyder <yder12354@gmail.com>
2025-05-23 16:23:55 -0500

    prueba 3

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,47 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../service/auth/auth.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  error: string = '';
 
-  isLoginMode: boolean = true;
+    constructor(private authService: AuthService,  private router: Router) {}
 
-  toggleMode() {
-    this.isLoginMode = !this.isLoginMode;}
+  onLogin() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        // Guarda el token o haz lo que necesites después del login exitoso
+        localStorage.setItem('token', res.token);
+        this.error = '';
+        alert('Login exitoso');
+        // Redirige a otra página si quieres
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.error = 'Credenciales incorrectas o error en el login';
+      },
+    });
+  }
 
-    onSubmit() {
-      if (this.isLoginMode) {
-        //this.authService.login(); // marcar como autenticado
-        //this.router.navigate(['/pagina-web']); // redirigir
-      } else {
-        // lógica de registro simulada
-        this.isLoginMode = true; // después de registrar, vuelve al login
-      }
-    }
+  isRegisterMode: boolean = true;
+  isLoginMode: boolean = false;
 
+  RegistertoggleMode() {
+    this.isRegisterMode = !this.isRegisterMode;}
+    
+  LoginToggleMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
+
---
+++ src/app/service/auth/auth.service.ts
@@ -0,0 +1,24 @@
+// auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface LoginResponse {
+  // Define aquí la respuesta que te envía el backend,
+  // por ejemplo un token JWT u otro dato
+  token: string;
+  // otros campos si hay
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://10.147.20.231:1901/api/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, { username, password });
+  }
+}
