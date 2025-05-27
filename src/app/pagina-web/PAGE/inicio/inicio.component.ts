be0e81aba6abb7b36c70437287178e559dd8fd70
FRETH LOPEZ <zepolfz18@gmail.com>
2025-05-27 10:23:33 -0500

    Aplicando API al inicio user

--- src/app/pagina-web/PAGE/inicio/inicio.component.ts
+++ src/app/pagina-web/PAGE/inicio/inicio.component.ts
@@ -1,14 +1,14 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProductoService, PublicacionesService } from '../../../service/pages/inicio.service';
+import { OnDestroy } from '@angular/core';
 
 interface Producto {
+  id: string;
   imagen: string;
   nombre: string;
-  subtitulo: string;
-  subtitulo2: string;
   descripcion: string;
-  enlaceTexto: string;
 }
 
 interface Publicacion {
@@ -32,83 +32,38 @@ export class InicioComponent implements OnInit {
   publicaciones: Publicacion[] = [];
   intervalId: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private productoService: ProductoService,
+    private publicacionesService: PublicacionesService
+  ) {}
 
   ngOnInit(): void {
-    // API de productos (descomentar si la tienes)
-    // this.http.get<Producto[]>('https://miapi.com/productos').subscribe(data => {
-    //   this.productos = data;
-    // });
-
-    this.productos = [
-      {
-        imagen: 'assets/imagenes/scomers.png',
-        nombre: 'Scomers 1',
-        subtitulo: 'Software de Comercializacion',
-        subtitulo2: 'Edición Empresarial',
-        descripcion: 'Sistema de Facturación y Control de Inventarios, fácil, robusto, confiable, eficiente y rápido. Contamos con la mejor garantía, nuestros clientes.',
-        enlaceTexto: 'Únete a nuestra red de clientes y disfruta como ellos las ventajas que brinda SCOMERS.'
-      },
-      {
-        imagen: 'assets/imagenes/scomers.png',
-        nombre: 'Scomers.CPE 2',
-        subtitulo: 'Facturacion Electronica',
-        subtitulo2: 'Sotfware de Facturacion',
-        descripcion: 'Sistema de Facturación Electronica, fácil, robusto, confiable, eficiente y rápido. Contamos con la mejor garantía, nuestros clientes.',
-        enlaceTexto: 'Únete a nuestra red de clientes y disfruta como ellos las ventajas que brinda SCOMERS.CPE'
-      },
-      {
-        imagen: 'assets/imagenes/scomers.png',
-        nombre: 'Scomers 3',
-        subtitulo: 'Software de Comercializacion',
-        subtitulo2: 'Edición Empresarial',
-        descripcion: 'Sistema de Facturación y Control de Inventarios, fácil, robusto, confiable, eficiente y rápido. Contamos con la mejor garantía, nuestros clientes.',
-        enlaceTexto: 'Únete a nuestra red de clientes y disfruta como ellos las ventajas que brinda SCOMERS.'
-      },
-      {
-        imagen: 'assets/imagenes/scomers.png',
-        nombre: 'Scomers.CPE 4',
-        subtitulo: 'Facturacion Electronica',
-        subtitulo2: 'Sotfware de Facturacion',
-        descripcion: 'Sistema de Facturación Electronica, fácil, robusto, confiable, eficiente y rápido. Contamos con la mejor garantía, nuestros clientes.',
-        enlaceTexto: 'Únete a nuestra red de clientes y disfruta como ellos las ventajas que brinda SCOMERS.CPE'
-      }
-    ];
-
-    this.publicaciones = [
-      {
-        imagen: 'assets/img/compra.png',
-        titulo: 'Ingresar Compras a Títulos Gratuitos (Costo “0.00”) 1',
-        fecha: '23 de Abril del 2025 10:34 am',
-        descripcion: 'En algunas ocasiones del mundo empresarial suele suceder operaciones especiales como los comprobantes con Costo Cero “0.00”.'
-      },
-      {
-        imagen: 'assets/img/correlativo.png',
-        titulo: 'Cómo corregir correlativo de series de comprobantes 2',
-        fecha: '21 de Abril del 2025 08:44 am',
-        descripcion: 'En esta opción se mostrará cómo corregir correlativos de comprobantes cuando sale el error de correlativo ya registrado.'
-      },
-      {
-        imagen: 'assets/img/error.png',
-        titulo: 'Tip: Corregir error al ejecutar el sistema YUPAY 3',
-        fecha: '18 de Abril del 2025 11:24 am',
-        descripcion: 'Procedimiento que te permitirá corregir el error cuando se ejecuta el sistema y por diferentes motivos no inicie o no aparezca nada.'
-      },
-      {
-        imagen: 'assets/img/correlativo.png',
-        titulo: 'Cómo corregir correlativo de series de comprobantes 4',
-        fecha: '21 de Abril del 2025 08:44 am',
-        descripcion: 'En esta opción se mostrará cómo corregir correlativos de comprobantes cuando sale el error de correlativo ya registrado.'
-      },
-      {
-        imagen: 'assets/img/error.png',
-        titulo: 'Tip: Corregir error al ejecutar el sistema YUPAY 5',
-        fecha: '18 de Abril del 2025 11:24 am',
-        descripcion: 'Procedimiento que te permitirá corregir el error cuando se ejecuta el sistema y por diferentes motivos no inicie o no aparezca nada.'
-      }
-    ];
-
+    this.getProducto();
+    this.getPublicacion();
     this.startAutoSlide();
+  }
+
+  getProducto(){
+    this.productoService.getProducto().subscribe({
+      next: (result) => {
+        this.productos = result.productos;
+      },
+      error: (error) => {
+        console.error('Error al obtener los productos', error);
+      }
+    });
+  }
+
+  getPublicacion(){
+    this.publicacionesService.getPublicacion().subscribe({
+      next: (result)=> {
+        console.log(result)
+        this.publicaciones = result.publicaciones;
+      },
+      error: (error)=> {
+        console.error('Error al obtener las publicaciones', error);
+      }
+    })
   }
 
   ngOnDestroy(): void {
